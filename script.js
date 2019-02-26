5f85ed9ed943262957faacc2cffdc2993450411e
mohd ayaz <ayansaifi970@gmail.com>
2019-02-26 15:59:39 +0530

    add new images and carousel

--- script.js
+++ script.js
@@ -1,27 +1,52 @@
 $(document).ready(function(){
     var w= $(window).width();
     console.log(w)
-});
-$(window).scroll(function() {
-    var hT = $('#partition-about').offset().top,
-        hH = $('#partition-about').outerHeight(),
+
+    $(window).scroll(function() {
+        var hT = $('#partition-about').offset().top,
+            hH = $('#partition-about').outerHeight(),
+            wH = $(window).height(),
+            wS = $(this).scrollTop();
+        if (wS > hT){
+            $('.partition-about').animate({height:'96px'}, 500);
+            $('.partition-about p span').css("font-size", "72px");
+            $('.partition-about p').css("font-size", "12px");
+        }
+    });
+
+    $(window).scroll(function() {
+        var hT = $('#partition-skills').offset().top,
+        hH = $('#partition-skills').outerHeight(),
         wH = $(window).height(),
         wS = $(this).scrollTop();
-    if (wS > (hT+hH-wH+500)){
-        $('.partition-about').animate({height:'96px'}, 500);
-        $('.partition-about p span').css("font-size", "72px");
-        $('.partition-about p').css("font-size", "12px");
+        if (wS > hT){
+            $('.partition-skills').animate({height:'96px'}, 500);
+            $('.partition-skills p span').css("font-size", "72px");
+            $('.partition-skills p').css("font-size", "12px");
+        }
+    });
 
+    $(window).scroll(function() {
+        var hT = $('#partition-work').offset().top,
+        hH = $('#partition-work').outerHeight(),
+        wH = $(window).height(),
+        wS = $(this).scrollTop();
+        if (wS > hT){
+            $('.partition-work').animate({height:'96px'}, 500);
+            $('.partition-work p span').css("font-size", "72px");
+            $('.partition-work p').css("font-size", "12px");
+        }
+    });
+
+    $('.photo-area').hide();
+
+    for (i=1; i<10; i++) {
+        $(".carousel-inner").append('<div class="item"><img src="ux'+i+'.jpg" alt="Chicago" style="width:50%;"></div>');
+        console.log(i)
     }
- });
- $(window).scroll(function() {
-    var hT = $('#partition-skills').offset().top,
-    hH = $('#partition-skills').outerHeight(),
-    wH = $(window).height(),
-    wS = $(this).scrollTop();
-if (wS > (hT+hH-wH+400)){
-    $('.partition-skills').animate({height:'96px'}, 500);
-    $('.partition-skills p span').css("font-size", "72px");
-    $('.partition-skills p').css("font-size", "12px");
-}
- });
+
+    $("#ux-image").click(function() {
+        $(".work-item").hide();
+        $(".photo-area").show();
+    });
+});
